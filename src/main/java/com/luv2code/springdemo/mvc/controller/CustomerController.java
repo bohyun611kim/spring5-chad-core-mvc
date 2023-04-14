dcc7e6b327abcea6772e9801dff4ff2861a614b0
bonggue <bohyun611.kim@gmail.com>
2023-04-14 14:38:34 +0900

    pre-processing @initBinder - whitespace, null trim : #01

--- src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.luv2code.springdemo.mvc.controller;
 
+
 import com.luv2code.springdemo.mvc.domain.Customer;
 import javax.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomerController {
 
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); // whitespace 제거
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
+
     @RequestMapping("/showForm")
-    public String showForm(Model theModel) {
+    public java.lang.String showForm(Model theModel) {
         System.out.println("--- /showForm");
         theModel.addAttribute("customer", new Customer());
 
@@ -21,8 +34,10 @@ public class CustomerController {
     }
 
     @RequestMapping("/processForm")
-    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+    public java.lang.String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
         BindingResult theBindingResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|"); // whitespace 검증을위함
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
